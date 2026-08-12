f838740ab6386b987e91856fb9feccc7df277812
Pissolato32 <rodrigopissolato@gmail.com>
2026-08-12 13:01:55 -0300

    refactor(ui): load design system foundation globally

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,8 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./App.js";
+import "./styles/variables.css";
+import "./styles/globals.css";
 import "./styles.css";
 import "./briefing/briefing.css";
 
