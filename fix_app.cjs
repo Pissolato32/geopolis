f244f7bc99117935fc79efa50c2027261cdfe346
Pissolato32 <84413717+Pissolato32@users.noreply.github.com>
2026-08-12 16:16:42 +0000

    feat(ui): refactor visual interface and UX to tactical intelligence dashboard

--- fix_app.cjs
+++
@@ -1,7 +0,0 @@
-const fs = require('fs');
-
-let app = fs.readFileSync('src/App.tsx', 'utf8');
-
-app = app.replace(/<GlobalSearch seed=\{seed\} \/>\n/g, '');
-
-fs.writeFileSync('src/App.tsx', app, 'utf8');
